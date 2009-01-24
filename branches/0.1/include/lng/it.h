c64f2b2af558662fa4c0cc36dc4faf5108ead243
finarfin <finarfin@c2713fe4-104e-0410-92db-9e4b537559a4>
2009-01-24 22:06:36 +0000

    Preparazione files per la 0.1

---
+++ branches/0.1/include/lng/it.h
@@ -0,0 +1,36 @@
+/*
+ * It.h
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#ifndef _IT_H_
+#define _IT_H_
+
+#define LNG_SITE "\nSito: "
+#define LNG_GDT "Inizializzo GDT   "
+#define LNG_IDT "Inizializzo IDT   "
+#define LNG_PAGING "Abilito Paging: "
+#define LNG_PIT8253 "Inizializzo PIT    "
+#define LNG_PIC8259 "Inizializzo PIC8259    "
+#define LNG_CPU "\nProcessore: "
+#define LNG_FREERAM "Ram Disposizione:"
+#define LNG_FREEPAGE "\t- Numero Pagine a disposizione:"
+#define LNG_BITMAP "\t- Numero elementi della bitmap:"
+#define LNG_SHELL "Carico la shell"
+#define LNG_USER "[?] Inserisci il nome utente: "
+#define LNG_USER_R "[x] Per favore, inserisci il nome utente :)\n"
+#define LNG_UNKNOWN_CMD "Comando sconosciuto:"
+
+#endif
---
+++ branches/0.1/include/use.h
@@ -0,0 +1,35 @@
+/*
+ * En.h
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+#ifndef _EN_H_
+#define _EN_H_
+
+#define LNG_SITE "\nSite: "
+#define LNG_GDT "Initialize GDT   "
+#define LNG_IDT "Initialize IDT   "
+#define LNG_PAGING "Enabling Paging: "
+#define LNG_PIT8253 "Initialize PIT    "
+#define LNG_PIC8259 "Initialize PIC8259    "
+#define LNG_CPU "\nCPU: "
+#define LNG_FREERAM "Free RAM:"
+#define LNG_FREEPAGE "\t- Number free page:"
+#define LNG_BITMAP "\t- Number bitmap's elements:"
+#define LNG_SHELL "Loading the shell"
+#define LNG_USER "[?] Enter your username: "
+#define LNG_USER_R "[x] Please, insert your username :)\n"
+#define LNG_UNKNOWN_CMD "Unknown command:"
+
+#endif
